a67f2eb757c29c36213612736fed94da8323e40b
BT_2S14_04 <@>
2025-03-11 10:57:43 -0400

    Code adjusted to work!

--- src/Check.java
+++ src/Check.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Check {
     private int[][] arr;
@@ -6,25 +7,26 @@ public class Check {
     public Check (int[][] array){
         arr = array;
     }
-// 123168
-    private int[] getColumn(){
+
+    private int[] getColumn(int i){
         int column[] = new int[arr.length];
-        for (int i = 0; i < arr[0].length; i++){
-            for (int j = 0; j < arr.length; j++){
-                column[j] = arr[i][j];
-            }
+        for (int j = 0; j < arr.length; j++){
+            column[j] = arr[j][i];
         }
-        System.out.println(column);
         return column;
     }
 
     public boolean duplicate(){
-        int[] column = getColumn();
-        for (int i = 0; i < column.length; i++){
-            int num = column[i];
-            for (int j = i+1; j < column.length; j++){
-                if (num == column[j]){
-                    return true;
+        for (int j = 0; j < arr.length; j++) {
+
+            for (int i = 0; i < arr.length; i++) {
+                int[] column = getColumn(j);
+                int num = column[i];
+
+                for (int k = i + 1; k < column.length; k++) {
+                    if (num == column[k]) {
+                        return true;
+                    }
                 }
             }
         }
